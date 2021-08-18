73d603574c54b13ee623c99771c28c3bdee89a54
Mohammad Sadeq Sirjani <m.sadeq.sirjani@gmail.com>
2021-08-18 18:24:39 +0330

    Add Project

---
+++ src/OperationResult/Result.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace OperationResult
+{
+    public readonly struct Result<T>
+    {
+        public T Value { get; }
+
+        public Exception Exception { get; }
+
+        public bool IsSuccess { get; }
+
+        public Result(T value)
+        {
+            Value = value;
+            Exception = null;
+            IsSuccess = true;
+        }
+
+        public Result(Exception exception)
+        {
+            Value = default;
+            Exception = exception ?? throw new ArgumentNullException(nameof(exception));
+            IsSuccess = false;
+        }
+
+        public static implicit operator Result<T>(T value) => new(value);
+
+        public static implicit operator Result<T>(Exception exception) => new(exception);
+
+        public Result<TEnd> ChangeInAnotherResult<TEnd>(Func<T, TEnd> converter)
+            => IsSuccess
+                ? new Result<TEnd>(converter(Value))
+                : new Result<TEnd>(Exception);
+
+        public Result ChangeInNoResult() =>
+            IsSuccess
+                ? new Result(true)
+                : new Result(Exception);
+
+        public void Deconstruct(out bool isSuccess, out T value)
+            => (isSuccess, value) = (IsSuccess, Value);
+
+        public void Deconstruct(out bool isSuccess, out T value, out Exception exception)
+            => (isSuccess, value, exception) = (IsSuccess, Value, Exception);
+
+        public static implicit operator bool(Result<T> result)
+            => result.IsSuccess;
+
+        public static implicit operator Task<Result<T>>(Result<T> result)
+            => result.AsTask;
+
+        public Task<Result<T>> AsTask => Task.FromResult(this);
+    }
+
+    public readonly struct Result
+    {
+        public Exception Exception { get; }
+
+        public bool IsSuccess { get; }
+
+        public Result(bool isSuccess)
+        {
+            Exception = null;
+            IsSuccess = isSuccess;
+        }
+
+        public Result(Exception exception)
+        {
+            Exception = exception ?? throw new ArgumentNullException(nameof(exception));
+            IsSuccess = false;
+        }
+
+        public bool ErrorIs<TException>() where TException : Exception => Exception is TException;
+
+        public static Result Success() => new(true);
+
+        public static Result<T> Success<T>(T value) => new(value);
+
+        public static Result Error(Exception exception) => new(exception);
+
+        public static Result<T> Error<T>(Exception exception) => new(exception);
+
+        public static implicit operator Result(Exception exception) => new(exception);
+
+        public static implicit operator bool(Result result) => result.IsSuccess;
+
+        public static implicit operator Task<Result>(Result result) => result.AsTask;
+
+        public void Deconstruct(out bool isSuccess, out Exception exception) =>
+            (isSuccess, exception) = (IsSuccess, Exception);
+
+        public Task<Result> AsTask => Task.FromResult(this);
+    }
+}
---
+++ test/OperationResult.Test/OperationResultTest.cs
@@ -0,0 +1,179 @@
+using FluentAssertions;
+using System;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace OperationResult.Test
+{
+    public class OperationResultTest
+    {
+        [Fact]
+        public void Result_Success_Return_True()
+        {
+            var result = Result.Success();
+
+            result.IsSuccess.Should().BeTrue();
+        }
+
+        [Fact]
+        public void Result_Error_Return_False()
+        {
+            var result = Result.Error(new Exception());
+
+            result.IsSuccess.Should().BeFalse();
+        }
+
+        [Fact]
+        public void Result_Should_Throw_Argument_Null_Exception_When_Pass_Null_Exception()
+        {
+            Action action = () => Result.Error(null);
+
+            action.Should().Throw<ArgumentNullException>();
+        }
+
+        [Fact]
+        public void ResultT_Should_Throw_Argument_Null_Exception_When_Pass_Null_Exception()
+        {
+            Action action = () => Result.Error<string>(null);
+
+            action.Should().Throw<ArgumentNullException>();
+        }
+
+        [Fact]
+        public void Result_Error_Return_Task_Result()
+        {
+            var result = Result.Error(new Exception());
+
+            result.AsTask.Should().BeOfType<Task<Result>>();
+            result.AsTask.Should().Awaiting(x =>
+            {
+                x.Subject.As<Result>().IsSuccess.Should().BeFalse();
+
+                return x.Subject.As<Result>();
+            });
+        }
+
+        [Fact]
+        public void Result_Success_Return_Task_Result()
+        {
+            var result = Result.Success();
+
+            result.AsTask.Should().BeOfType<Task<Result>>();
+            result.AsTask.Should().Awaiting(x =>
+            {
+                x.Subject.As<Result>().IsSuccess.Should().BeTrue();
+
+                return x.Subject.As<Result>();
+            });
+        }
+
+        [Fact]
+        public void Change_Type_Of_T_Using_ChangeInAnotherResult()
+        {
+            var value = new
+            {
+                Id = Guid.NewGuid(),
+                Name = Guid.NewGuid().ToString("N")[15..]
+            };
+
+            var result = Result.Success(value).ChangeInAnotherResult(x => new TemplateClass(x.Id, x.Name));
+
+            result.Should().BeOfType<Result<TemplateClass>>();
+            result.IsSuccess.Should().BeTrue();
+            result.Value.Should().BeOfType<TemplateClass>();
+        }
+
+        [Fact]
+        public void Change_Type_Of_ResultT_To_Result()
+        {
+            var value = new TemplateClass
+            {
+                Id = Guid.NewGuid(),
+                Name = Guid.NewGuid().ToString("N")[15..]
+            };
+
+            var result = Result.Success(value).ChangeInNoResult();
+
+            result.Should().BeOfType<Result>();
+            result.IsSuccess.Should().BeTrue();
+            result.Exception.Should().BeNull();
+        }
+
+        [Fact]
+        public void Deconstruct_Result()
+        {
+            var value = new TemplateClass
+            {
+                Id = Guid.NewGuid(),
+                Name = Guid.NewGuid().ToString("N")[15..]
+            };
+
+            var result = Result.Success(value);
+
+            var (isSuccess, entity, exception) = result;
+
+            isSuccess.Should().BeTrue();
+            entity.Should().NotBeNull();
+            entity.Should().BeOfType<TemplateClass>();
+            exception.Should().BeNull();
+
+            (isSuccess, entity) = result;
+
+            isSuccess.Should().BeTrue();
+            entity.Should().NotBeNull();
+            entity.Should().BeOfType<TemplateClass>();
+        }
+
+        [Fact]
+        public void Result_Should_Pass_True_Or_False_In_Condition()
+        {
+            var result = Result.Success();
+
+            bool isSuccess = result;
+
+            isSuccess.Should().BeTrue();
+            ((bool)result).Should().BeTrue();
+
+            result = Result.Error(new Exception());
+
+            bool isFailure = result;
+
+            isFailure.Should().BeFalse();
+            ((bool)result).Should().BeFalse();
+
+            var newResult = Result.Error<string>(new Exception());
+
+            isFailure = newResult;
+
+            isFailure.Should().BeFalse();
+            ((bool)newResult).Should().BeFalse();
+        }
+
+        [Fact]
+        public void Result_Should_Cast_To_Task()
+        {
+            var result = Result.Success();
+
+            Task operation = result;
+
+            operation.GetAwaiter().IsCompleted.Should().BeTrue();
+        }
+
+        private class TemplateClass
+        {
+            public TemplateClass()
+            {
+
+            }
+
+            public TemplateClass(Guid id, string name)
+            {
+                Id = id;
+                Name = name;
+            }
+
+            public Guid Id { get; init; }
+            public string Name { get; init; }
+        }
+    }
+}
